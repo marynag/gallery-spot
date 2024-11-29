579fd1e3097b9a798d44ae6d8b2da727632e15a8
marynag <mgrabchak1109@gmail.com>
2024-11-29 21:32:45 +0200

    display fetched photos

---
+++ src/types/types.ts
@@ -0,0 +1,33 @@
+interface Urls {
+  raw: string;
+  full: string;
+  regular: string;
+  small: string;
+  thumb: string;
+  small_s3: string;
+}
+
+interface IUser {
+  id: string;
+  username: string;
+  name: string;
+  first_name: string;
+  last_name: string;
+  portfolio_url: string;
+}
+
+export interface IGallery {
+  id: string;
+  slug: string;
+  created_at: string;
+  promoted_at: string | null;
+  width: number;
+  height: number;
+  color: string;
+  description: string | null;
+  urls: Urls;
+  likes: number;
+  topic_submissions: any;
+  asset_type: string;
+  user: IUser;
+}
